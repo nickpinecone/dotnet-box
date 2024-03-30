d1925705f888a31bfa99a0750f89d0cb27d86990
threedots <threedotsellipsis@protonmail.com>
2024-03-30 17:52:02 +0500

    Complete basic certificate api

--- server/api/portfolios.route.ts
+++ server/api/portfolios.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import Portfolio from "../models/portfolio.model";
 import path from "path";
+import fs from "fs";
 
 const router = express.Router();
 
@@ -11,6 +12,11 @@ router.route("/").get(async (req, res) => {
     const portfolios = await Portfolio.find({}).sort({ createdAt: -1 });
 
     res.send(portfolios);
+});
+
+router.route("/").post(async (req, res) => {
+    await Portfolio.create({});
+    res.sendStatus(200);
 });
 
 router.route("/:id").get(async (req, res) => {
@@ -27,12 +33,11 @@ router.route("/:id").get(async (req, res) => {
     }
 });
 
-router.route("/:id/project").post(upload.none(), async (req, res) => {
+router.route("/:id/project").get(async (req, res) => {
     try {
-        const body = req.body;
         const portfolio = await Portfolio.findOne({ _id: req.params.id });
 
-        const project = { description: body.description, url: body.url };
+        const project = { description: "", url: "" };
         const count = portfolio?.projects.push(project);
         await portfolio?.save();
 
@@ -86,31 +91,82 @@ router.route("/:id/project/:projectId").delete(async (req, res) => {
     }
 });
 
+router.route("/:id/certificate").get(async (req, res) => {
+    try {
+        const portfolio = await Portfolio.findOne({ _id: req.params.id });
 
-// TODO do same with certifiaces
+        const certifiace = { description: "", photo: "" };
+        const count = portfolio?.certificates.push(certifiace);
+        await portfolio?.save();
 
-// router.route("/:id/project/:projectId").patch(async (req, res) => {
-//     const portfolio = await Portfolio.findOne({ _id: req.params.id });
-//     const project = portfolio?.projects.find((one) => one._id == req.params.projectId);
-// });
+        res.send(portfolio?.certificates[(count as number) - 1]);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("could not create certificate in portfolio with id: " + req.params.id);
+    }
+});
 
-// router.route("/:id/certificate").patch(upload.single("certificate"), async (req, res) => {
-//     try {
-//         if (!req.file) throw new Error("did not recieve photo");
+router.route("/:id/certificate/:certificateId").put(upload.single("certificate"), async (req, res) => {
+    try {
+        const body = req.body;
+        const portfolio = await Portfolio.findOne({ _id: req.params.id });
 
-//         const body = req.body;
-//         const portfolio = await Portfolio.findOne({ _id: req.params.id });
+        const certifiace = portfolio?.certificates.find((one) => one._id?.toString() == req.params.certificateId);
 
-//         const certifiace = { description: body.description, photo: req.file.filename };
-//         portfolio?.certificates.push(certifiace);
-//         portfolio?.save();
+        if (certifiace && req.file) {
+            certifiace.description = body.description;
+            certifiace.photo = req.file.filename;
 
-//         res.sendStatus(200);
-//     }
-//     catch (err) {
-//         console.error(err);
-//         res.status(500).send("couldnt update portfolio with id: " + req.params.id);
-//     }
-// });
+            await portfolio?.save();
+        }
+        else {
+            throw new Error();
+        }
+
+        res.sendStatus(200);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("could not update certifiace with id: " + req.params.certificateId);
+    }
+});
+
+// Get certifiace photo
+router.route("/:id/certificate/:certificateId/photo").get(async (req, res) => {
+    try {
+        const portfolio = await Portfolio.findOne({ _id: req.params.id });
+
+        const certifiace = portfolio?.certificates.find((one) => one._id?.toString() == req.params.certificateId);
+
+        if (!certifiace) throw new Error();
+
+        const photoName = path.resolve(__dirname, "..", "public/certificates/" + certifiace?.photo);
+        const readStream = fs.createReadStream(photoName);
+        res.status(200);
+        readStream.pipe(res);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("could not get photo for certificate with id: " + req.params.certificateId);
+    }
+});
+
+router.route("/:id/certificate/:certificatedId").delete(async (req, res) => {
+    try {
+        const portfolio = await Portfolio.findOne({ _id: req.params.id });
+
+        const certifiace = portfolio?.certificates.find((one) => one._id?.toString() == req.params.certificatedId);
+        portfolio?.projects.remove(certifiace);
+
+        await portfolio?.save();
+
+        res.sendStatus(200);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("could not delete certifiace with id: " + req.params.certificatedId);
+    }
+});
 
 export default router;
