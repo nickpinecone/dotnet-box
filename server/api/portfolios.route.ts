092ab9f22637a6aa8d1f3bb878d1cd0f74f441d9
threedots <threedotsellipsis@protonmail.com>
2024-04-21 18:11:51 +0500

    Also search in project and certificate desc

--- server/api/portfolios.route.ts
+++ server/api/portfolios.route.ts
@@ -9,18 +9,50 @@ import User from "../models/user.model";
 
 import validation from "../middlewares/validate.middleware";
 import { query } from "express-validator";
+import Project from "../models/project.model";
+import Certificate from "../models/certificate.model";
 
 const router = express.Router();
 const upload = multer();
 
 router.route("/").get(
-    query("search").default("").escape(),
+    query("search").optional().escape(),
     validation.validateForm, async (req, res) => {
         try {
-            const searchKey = new RegExp(`${req.query.search}`, 'i');
-            const portfolios = await Portfolio.find({ "description": searchKey }).sort({ createdAt: -1 }).populate("owner projects certificates");
+            if (req.query.search) {
+                const searchList = [];
 
-            res.send(portfolios);
+                const searchKey = new RegExp(`${req.query.search}`, 'i');
+                const searchSettings = { "description": searchKey };
+
+                const portfolios = await Portfolio.find(searchSettings).sort({ createdAt: -1 }).populate("owner projects certificates");
+                for (let i = 0; i < portfolios.length; i++) {
+                    const portfolio = portfolios[i];
+                    searchList.push(portfolio);
+                }
+
+                const projects = await Project.find(searchSettings).sort({ createdAt: -1 });
+                for (let i = 0; i < projects.length; i++) {
+                    const project = projects[i];
+                    const portfolio = await Portfolio.findById(project.portfolio).populate("owner projects certificates");
+                    if (portfolio && searchList.every((item) => item._id.toString() !== portfolio._id.toString()))
+                        searchList.push(portfolio);
+                }
+
+                const certificates = await Certificate.find(searchSettings).sort({ createdAt: -1 });
+                for (let i = 0; i < certificates.length; i++) {
+                    const certificate = certificates[i];
+                    const portfolio = await Portfolio.findById(certificate.portfolio).populate("owner projects certificates");
+                    if (portfolio && searchList.every((item) => item._id.toString() !== portfolio._id.toString()))
+                        searchList.push(portfolio);
+                }
+
+                res.send([...searchList]);
+            }
+            else {
+                const portfolios = await Portfolio.find({}).sort({ createdAt: -1 }).populate("owner projects certificates");
+                res.send(portfolios);
+            }
         }
         catch (err) {
             console.error(err);
