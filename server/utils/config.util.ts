968f95040ee5d7f0daad3155bfb3cd03eb038900
nickpinecone <nick.pineconefir@gmail.com>
2025-04-12 21:49:22 +0500

    Clean up

--- server/utils/config.util.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const isDevelopment = process.env.MODE === "dev";
-
-export const siteUrl = isDevelopment ? "localhost:3000" : "https://threedotsellipsis.github.io/digital-portfolio/";
-export const serverUrl = isDevelopment ? "localhost:4000" : "185.211.170.35";
